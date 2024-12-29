7e012056424852b8a4f2fc7caca66dbd3f3cbe5d
Robert Donnelly <dev@robdonn.com>
2024-12-29 13:43:24 +0000

    avatars

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { X } from 'lucide-react';
 import { usePartyMembers } from '../hooks/usePartyMembers';
-import { Button } from './ui/button';
+import { PartyMember } from './PartyMember';
+import { PresetMember } from '../types/Member';
 
 export const Chat = () => {
   const { partyMembers, removeMember } = usePartyMembers();
@@ -11,19 +11,9 @@ export const Chat = () => {
       <li className="video bg-red-300 text-center aspect-video min-h-40 max-h-60">
         User
       </li>
-      {partyMembers.map((member: string) => (
-        <li
-          key={member}
-          className="video bg-red-300 text-center aspect-video min-h-40 max-h-60 flex flex-col items-center"
-        >
-          {member}
-          <Button
-            onClick={() => removeMember(member)}
-            variant="default"
-            className="inline-flex h-12 w-12 items-center justify-center rounded-full"
-          >
-            <X />
-          </Button>
+      {partyMembers.map((member: PresetMember['id']) => (
+        <li key={member}>
+          <PartyMember id={member} />
         </li>
       ))}
     </ul>
---
+++ src/components/PartyMember.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { X } from 'lucide-react';
+import { Button } from './ui/button';
+import { usePartyMembers } from '../hooks/usePartyMembers';
+import { PresetMember } from '../types/Member';
+
+import { avatars } from '../data/avatars';
+
+export const PartyMember: React.FC<{ id: PresetMember['id'] }> = ({ id }) => {
+  const { removeMember } = usePartyMembers();
+
+  const avatar = avatars.find((avatar) => avatar.id === id);
+
+  return (
+    <div className="flex flex-col items-center relative">
+      <img
+        src={`/static/avatars/${avatar?.avatar}.jpg`}
+        className="video bg-red-300 text-center aspect-square min-h-40 max-h-60 object-cover"
+      />
+      <Button
+        onClick={() => removeMember(id)}
+        variant="secondary"
+        className="inline-flex h-12 w-12 items-center justify-center rounded-full absolute bottom-4"
+      >
+        <X />
+      </Button>
+    </div>
+  );
+};
